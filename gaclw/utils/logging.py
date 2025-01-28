1ebdf3193dd7fc9bf4abbe0a4e865925c5b35c90
aYukiYoshida <yuki.yoshida@access-company.com>
2025-01-28 10:49:13 +0900

    fix: modify multiple logging output

--- gaclw/utils/logging.py
+++ gaclw/utils/logging.py
@@ -15,15 +15,16 @@ def get_logger(level: int = settings.LOG_LEVEL) -> logging.Logger:
     """
     logger = logging.getLogger(__name__)
     logger.setLevel(level * 10)
-    formatter = logging.Formatter(
-        "%(asctime)s [%(levelname)s] %(message)s",
-        datefmt=r"%Y-%m-%dT%H:%M:%S%z",
-    )
-    # ログを標準出力する
-    handler = logging.StreamHandler(stream=sys.stdout)
-    handler.setLevel(logging.DEBUG)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    if not logger.hasHandlers():
+        # ログを標準出力する
+        handler = logging.StreamHandler(stream=sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+            "%(asctime)s [%(levelname)s] %(message)s",
+            datefmt=r"%Y-%m-%dT%H:%M:%S%z",
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
     return logger
 
 
